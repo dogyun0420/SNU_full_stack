50538371289c4f9800da77e8613af16a3b75f275
김도균 <bogyun0420@gmail.com>
2025-02-06 11:21:20 +0900

    add js

---
+++ static/about.js
@@ -0,0 +1,17 @@
+// 페이지가 로드될 때 실행되는 함수
+window.onload = function() {
+    console.log("About page loaded");
+
+    // 페이지 내 버튼 클릭 시 알림 메시지 표시
+    const homeButton = document.querySelector('.button');
+    if (homeButton) {
+        homeButton.addEventListener('click', function(event) {
+            alert("You are going back to the homepage!");
+        });
+    }
+
+    // 페이지 로드 후 2초 뒤에 안내 메시지 표시
+    setTimeout(function() {
+        alert("Welcome to the Netflix API About Page!");
+    }, 2000);
+}
