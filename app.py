f47d46a76e95aad483aa311afcb4a4693535996c
김도균 <bogyun0420@gmail.com>
2025-02-06 12:03:52 +0900

    today end

--- app.py
+++ app.py
@@ -44,3 +44,6 @@ def api():
         "count": len(results),
         "titles": results
     }
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
--- static/about.js
+++ static/about.js
@@ -4,9 +4,15 @@ window.onload = function() {
 
     // 페이지 내 버튼 클릭 시 알림 메시지 표시
     const homeButton = document.querySelector('.button');
+    // if (homeButton) {
+    //     homeButton.addEventListener('click', function(event) {
+    //         alert("You are going back to the homepage!");
+    //     });
+    // }
     if (homeButton) {
-        homeButton.addEventListener('click', function(event) {
-            alert("You are going back to the homepage!");
+        homeButton.addEventListener('dblclick', function(event) {
+            alert("You double-clicked to go back to the homepage!");
+            console.log("dbdbdb");
         });
     }
 
